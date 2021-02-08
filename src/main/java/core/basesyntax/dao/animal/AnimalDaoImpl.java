a86cee2cae8000ce3bc9c8d98cd6fa8a5f1588bf
Serhii Semenykhin <semyonich@users.noreply.github.com>
2021-02-08 11:48:52 +0200

    warnings fixed

--- src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
+++ src/main/java/core/basesyntax/dao/animal/AnimalDaoImpl.java
@@ -1,6 +1,8 @@
 package core.basesyntax.dao.animal;
 
 import core.basesyntax.dao.AbstractDao;
+import core.basesyntax.model.zoo.Animal;
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
